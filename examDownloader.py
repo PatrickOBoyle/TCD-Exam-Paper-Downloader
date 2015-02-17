59e649731a7b18371998ecdb87fc7e63ceb33f43
Patrick O'Boyle <oboylepa@tcd.ie>
2015-02-17 23:22:24 +0000

    Commented code
    - Added comments to the code.
    - Improved readability with spacing.
    - Added a Created/Last updated comment.

--- examDownloader.py
+++ examDownloader.py
@@ -1,6 +1,10 @@
+# Created:		 17/02/2015
+# Last updated:	 17/02/2015
+
 import urllib2
 from HTMLParser import HTMLParser
 
+# URLs for constructing the link to exam papers
 base_url = "https://www.tcd.ie/Local/Exam_Papers/annual_search.cgi?"
 exam_year = "&acyear="
 course_year = "&Standing="
@@ -8,10 +12,13 @@ course_id = "Course=19"
 end_url = "&annual_search.cgi=Search"
 TCD_URL = "https://www.tcd.ie"
 
+# Used to block downloads on data params after the initial name of the file
 canDownload = True
 
+# Sets both strings as empty initially
 download_url = ""
 file_name = ""
+
 
 print("Enter the year of exams you want:")
 
@@ -23,8 +30,10 @@ print("Enter your course year: (1 = JF, 2 = SF, 3 = JS, 4 = SS)")
 user_year = raw_input()
 course_year += user_year
 
+# Creates the URL to the page the documents are linked on
 complete_year = base_url + course_id + exam_year + course_year + end_url
 
+# Dump the HTML from the page into a string
 response = urllib2.urlopen(complete_year)
 html = response.read()
 
@@ -38,7 +47,9 @@ def download_file(download_url, file_name):
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
+    	# Checks to see if the tag is an anchor tag
         if(tag == "a"):
+        	# Tests if the value of the tuple starts with a forward slash (start address of all exam paper URLs)
         	if(attrs[0][1].startswith("/")):
         		partial_url = attrs[0][1]
         		global download_url
@@ -48,19 +59,29 @@ class MyHTMLParser(HTMLParser):
 
 
     def handle_data(self, data):
+    	# Skips over all data returns that are empty
     	if(data != ""):
     		global file_name
         	file_name = data + ".pdf"
+
+        	# Block of tests to weed out static element on all the download pages, blocks downloading the same file under multiple names
         	if(file_name.startswith("T")):
         		file_name = ""
+
         	if(file_name.startswith("A")):
         		file_name = ""
+
         	if(file_name.startswith("Y")):
         		file_name = ""
+
+        	
         	if(download_url != ""):
         		global canDownload
+
         		if(canDownload == True):
         			download_file(download_url, file_name)
+
+        			# After a download occurs, block downloading another file until a new URL has been assigned
         			canDownload = False;
 
 parser = MyHTMLParser()
